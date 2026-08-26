245a5ee1bca8cc66b612f05121fee657ff045d13
Nydeef.Taylor <tnydeef16@gmail.com>
2026-08-26 19:06:39 -0400

    Added README and initial project files

---
+++ src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,9 @@
+package org.howard.edu.lsp.assignment1;
+/**
+ * Hello World program for CSCI 363.
+ * @author Nydeef Taylor
+ */
+public class HelloWorld {public static void main(String[] args) {
+    System.out.println("Hello World!!");
+}
+}
